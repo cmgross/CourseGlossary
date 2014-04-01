6ea771a8f5f409c08b8eb06b6407859f42d991f2
Uziel <chuckgross@gmail.com>
2014-03-31 20:02:27 -0400

    Added menu system based on authorization

--- CourseGlossary/Controllers/ChapterController.cs
+++ CourseGlossary/Controllers/ChapterController.cs
@@ -37,19 +37,6 @@ namespace CourseGlossary.Controllers
          [HttpPost]
          public ActionResult Create(Chapter chapter)
          {
-             //TODO create menu hierarchy like audit site:
-         //http://stackoverflow.com/questions/6981853/asp-net-mvc3-razor-display-actionlink-based-on-user-role
-              //@if (Helpers.IsAdmin(User.Identity.Name))
-              //      {
-              //          <li class="dropdown">
-              //              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin<b class="caret"></b></a>
-              //              <ul class="dropdown-menu">
-              //                  <li>@Html.ActionLink("Users", "Index", "User")</li>
-              //                  <li>@Html.ActionLink("Managers", "Index", "Manager")</li>
-              //                  <li>@Html.ActionLink("Categories", "Index", "ErrorCategory")</li>
-              //              </ul>
-              //          </li>
-              //      }
              if (ModelState.IsValid)
              {
                  try
